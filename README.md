a1456b89c134cd0cdd110e8b67a94a445213af22
antony <antony.leo12agosto@gmail.com>
2022-04-29 18:38:47 -0500

    pages

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt text](public/img/caculadora.png)
--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,14 @@
-const prev = document.querySelector('.prev-value')
 const input = document.getElementById('input__screen2')
 
+input.addEventListener('load', (e)=>{
+  input.value = 0
+})
+
 const calculator = {
-  prevState: 0,
-  currentState: 0,
-  result: '',
+  flag: false,
+  prev: 0,
+  current: 0,
+  result: 0,
   operation: '',
   histoarial: [],
   operations: {
@@ -14,7 +18,13 @@ const calculator = {
     '/': function (prev, current) { calculator.result = prev / current },
     '^': function (prev, current) { calculator.result = prev ** current }
   },
-  input: (e) => { input.value += e.target.textContent },
+  input: (e) => {
+    if (calculator.flag) {
+      calculator.flag = false
+      input.value = ''
+    }
+    input.value += e.target.textContent
+  },
   delete: () => { input.value = input.value.slice(0, -1) },
   reset: () => borrar(),
   addOperator: (e) => addOperator(e),
@@ -22,28 +32,24 @@ const calculator = {
 }
 
 function addOperator (e) {
-  !calculator.operation || makeOp()
-  calculator.prevState = parseFloat(input.value) || 0
+  calculator.flag = true
   calculator.operation = e.target.textContent
-  prev.textContent = calculator.prevState + ' ' + calculator.operation + ' '
-  input.value = ''
+  calculator.current = parseFloat(input.value)
 }
 
 function makeOp () {
-  const key = calculator.operation
-  calculator.operation = ''
+  calculator.flag = true
+  calculator.prev = calculator.current
   calculator.current = parseFloat(input.value)
-  calculator.operations[key](calculator.prevState, calculator.current)
-  prev.textContent += calculator.current
+  calculator.operations[calculator.operation](calculator.prev, calculator.current)
   input.value = calculator.result
 }
 
 function borrar () {
-  prev.textContent = ''
-  input.value = ''
+  input.value = 0
   calculator.result = ''
   calculator.prevState = ''
-  calculator.currentState = ''
+  calculator.current = ''
   calculator.operation = ''
 }
 
